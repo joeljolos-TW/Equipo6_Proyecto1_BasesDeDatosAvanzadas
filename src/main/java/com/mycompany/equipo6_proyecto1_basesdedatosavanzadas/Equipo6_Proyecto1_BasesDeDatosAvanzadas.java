4bbaf2dec3f3c0a68761dcad485b5553073c31cd
joeljolos-TW <joel.cabe2403@gmail.com>
2025-06-14 10:28:32 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/equipo6_proyecto1_basesdedatosavanzadas/Equipo6_Proyecto1_BasesDeDatosAvanzadas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.equipo6_proyecto1_basesdedatosavanzadas;
+
+/**
+ *
+ * @author joel_
+ */
+public class Equipo6_Proyecto1_BasesDeDatosAvanzadas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
